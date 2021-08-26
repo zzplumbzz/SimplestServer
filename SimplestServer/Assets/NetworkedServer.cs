51bc4449ca95f892ef1a96b7f7f4492f112dd4cf
Fernando <linksgreenhat@gmail.com>
2021-08-26 13:38:17 -0400

    clean up complete

---
+++ SimplestServer/Assets/NetworkedServer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+using UnityEngine.UI;
+
+public class NetworkedServer : MonoBehaviour
+{
+    int maxConnections = 1000;
+    int reliableChannelID;
+    int unreliableChannelID;
+    int hostID;
+    int socketPort = 5491;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NetworkTransport.Init();
+        ConnectionConfig config = new ConnectionConfig();
+        reliableChannelID = config.AddChannel(QosType.Reliable);
+        unreliableChannelID = config.AddChannel(QosType.Unreliable);
+        HostTopology topology = new HostTopology(config, maxConnections);
+        hostID = NetworkTransport.AddHost(topology, socketPort, null);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int recHostID;
+        int recConnectionID;
+        int recChannelID;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error = 0;
+
+        NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recHostID, out recConnectionID, out recChannelID, recBuffer, bufferSize, out dataSize, out error);
+
+        switch (recNetworkEvent)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                Debug.Log("Connection, " + recConnectionID);
+                break;
+            case NetworkEventType.DataEvent:
+                string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                ProcessRecievedMsg(msg, recConnectionID);
+                break;
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log("Disconnection, " + recConnectionID);
+                break;
+        }
+
+    }
+  
+    public void SendMessageToClient(string msg, int id)
+    {
+        byte error = 0;
+        byte[] buffer = Encoding.Unicode.GetBytes(msg);
+        NetworkTransport.Send(hostID, id, reliableChannelID, buffer, msg.Length * sizeof(char), out error);
+    }
+    
+    private void ProcessRecievedMsg(string msg, int id)
+    {
+        Debug.Log("msg recieved = " + msg + ".  connection id = " + id);
+    }
+
+}
